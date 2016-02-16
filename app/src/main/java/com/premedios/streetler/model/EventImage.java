30a757de3f093297a35c06aeeefad1ecdfbf004e
premedios <pedro.remedios@gmail.com>
2016-02-16 01:24:57 +0000

    Database structure changes

--- app/src/main/java/com/premedios/streetler/model/EventImage.java
+++ app/src/main/java/com/premedios/streetler/model/EventImage.java
@@ -1,52 +1,19 @@
 package com.premedios.streetler.model;
 
+import com.activeandroid.annotation.Column;
+import com.activeandroid.annotation.Table;
+
 /**
  * Created by user on 12/02/2016.
  */
+
+@Table(name = "Event Images")
+
 public class EventImage {
-    private int id;
-    private byte[] image;
-    private Event eventId;
-    private User userId;
+    @Column(name = "image")
+    public byte[] image;
 
-    public EventImage() {}
+    @Column(name = "user")
+    private User user;
 
-    public EventImage(int id, byte[] image, Event eventId, User userId) {
-        this.id = id;
-        this.image = image;
-        this.eventId = eventId;
-        this.userId = userId;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public byte[] getImage() {
-        return image;
-    }
-
-    public void setImage(byte[] image) {
-        this.image = image;
-    }
-
-    public Event getEventId() {
-        return eventId;
-    }
-
-    public void setEventId(Event eventId) {
-        this.eventId = eventId;
-    }
-
-    public User getUserId() {
-        return userId;
-    }
-
-    public void setUserId(User userId) {
-        this.userId = userId;
-    }
 }
