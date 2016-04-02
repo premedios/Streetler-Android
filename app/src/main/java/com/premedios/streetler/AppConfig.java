2db4ec30689d9e42f0c8dd2b9ad673091bc79ce0
Pedro Remedios <pedro.remedios@gmail.com>
2016-04-03 00:01:54 +0100

    Register screen added

---
+++ app/src/main/java/com/premedios/streetler/AppConfig.java
@@ -0,0 +1,13 @@
+package com.premedios.streetler;
+
+/**
+ * Created by Peter on 02-04-2016.
+ */
+
+public class AppConfig {
+    // Server user login url
+    public static String URL_LOGIN = "http://streetler-test.net16.net/login.php";
+
+    // Server user register url
+    public static String URL_REGISTER = "http://streetler-test.net16.net/register.php";
+}
--- app/src/main/java/com/premedios/streetler/LoginActivity.java
+++ app/src/main/java/com/premedios/streetler/LoginActivity.java
@@ -7,6 +7,7 @@ public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        final String test;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
     }
