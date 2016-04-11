0c1f90b818173ea22f2112d664e7fce0086f62f4
premedios <pedro.remedios@gmail.com>
2016-04-11 01:08:15 +0100

    Login and Registration code

---
+++ app/src/main/java/com/premedios/streetler/CreateEventActivity.java
@@ -0,0 +1,13 @@
+package com.premedios.streetler;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class CreateEventActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_event);
+    }
+}
