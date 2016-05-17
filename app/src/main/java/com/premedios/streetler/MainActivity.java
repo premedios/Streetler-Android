620614f380de4661fedcc01dbb08685146d88fd5
premedios <pedro.remedios@gmail.com>
2016-05-17 01:28:26 +0100

    Changes

--- app/src/main/java/com/premedios/streetler/MainActivity.java
+++ app/src/main/java/com/premedios/streetler/MainActivity.java
@@ -3,6 +3,7 @@ package com.premedios.streetler;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,9 +14,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         TextView MainTitle = (TextView) findViewById(R.id.main_title);
+        Button registerButton = (Button) findViewById(R.id.register_button);
+        Button loginButton = (Button) findViewById(R.id.login_button);
         Typeface typeface = Typeface.createFromAsset(getAssets(), "fonts/Exo2-SemiBold.otf");
         if (MainTitle != null) {
             MainTitle.setTypeface(typeface);
+            registerButton.setTypeface(typeface);
+            loginButton.setTypeface(typeface);
         }
     }
 }
